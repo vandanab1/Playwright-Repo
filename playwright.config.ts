5d0159984424817e9c4933f13e8782b26efa3bac
vandanab1 <vandanasharma.b1@gmail.com>
2026-03-19 19:54:36 +0530

    comment

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
    reporter: [
     ['line'], // clean console logs in CI
-    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+    ['html'], 
     ['allure-playwright']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
