ad158b9d7e81d2330b024bfcbafe2969d054b67a
vandanab1 <vandanasharma.b1@gmail.com>
2026-03-20 14:56:55 +0530

    updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
    reporter: [
-    ['list'], // clean console logs in CI
+    ['list'],
     ['allure-playwright']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,8 +1,4 @@
-//const {test, expect}= require('@playwright/test');
 const { test, expect } = require('@fixtures/myFixtures');
-//const { LoginPage} = require('@pages/login.page');
-//const { ProductPage } = require('@pages/product.page');
-
 test('Valid login', async({loginPage})=>{
 await loginPage.goto();
 await loginPage.login("standard_user","secret_sauce");
