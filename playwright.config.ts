22c6b5dc1665907775e2d98c6e77162984c2674a
vandanab1 <vandanasharma.b1@gmail.com>
2026-03-20 14:33:56 +0530

    updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,8 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
    reporter: [
-    ['line'], // clean console logs in CI
-    ['html'], 
+    ['list'], // clean console logs in CI
     ['allure-playwright']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
