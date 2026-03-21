3d6e1eaac55fecf7045000a43f282c3aedd67780
vandanab1 <vandanasharma.b1@gmail.com>
2026-03-21 12:32:20 +0530

    Added condition in product test

--- tests/product.spec.js
+++ tests/product.spec.js
@@ -1,9 +1,10 @@
 const { test, expect } = require('@fixtures/myFixtures');
-//const loginData = require('@data/loginData.json');
+const loginData = require('@data/loginData.json');
 
 test('Validation product', async({loginPage,productPage})=>{
+const validUser = loginData.find(u => u.type === 'valid');
 await loginPage.goto();
-await loginPage.login("standard_user","secret_sauce");
+await loginPage.login(validUser.username, validUser.password);
 await expect(productPage.getProduct()).toHaveCount(6);
 await expect(productPage.options).toHaveCount(4);
 await productPage.dropdown.selectOption({label:'Name (A to Z)'});
